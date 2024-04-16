40e9083fa2d68328c6dc1dcff7db0023b44b3f13
kimnle <kimnle002@gmail.com>
2024-04-16 20:57:51 +1000

    oop and file handling

---
+++ file_handling.py
@@ -0,0 +1,11 @@
+readme_file = open("readme.txt", "a")
+
+# print(readme_file.read())
+
+# readme_file.write("write this at the end of the file")
+
+# print(readme_file.read())
+
+readme_file.close()
+
+# readme_file.write("hello again")
